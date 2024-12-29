322306139d382b10a1c173a24fa386d2d0f72704
bananza13 <164437055+bananza13@users.noreply.github.com>
2024-12-30 02:44:02 +0900

    Add files via upload

---
+++ content/text.md
@@ -0,0 +1,3 @@
+Title: My Post Pelican 
+Date: 16-11-2024 
+Author: Aleksandr Osipov 
---
+++ script.py
@@ -0,0 +1,4 @@
+output_file_path = 'output/results.txt'
+
+with open(output_file_path, 'w') as f:
+    f.write("Это выходные данные.")
